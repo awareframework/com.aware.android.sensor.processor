9b92f048e7e7117a3444a89ae13c6a60599b6352
Sercan Turkmen <sercanturkmen@outlook.com>
2018-07-27 11:23:40 +0300

    Change test to kotlin

---
+++ library/src/androidTest/java/com/awareframework/android/sensor/processor/ExampleInstrumentedTest.kt
@@ -0,0 +1,42 @@
+package com.awareframework.android.sensor.processor;
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.awareframework.android.core.db.Engine
+import com.awareframework.android.sensor.processor.model.ProcessorData
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.aware.android.sensor.processor.test", appContext.packageName)
+
+        ProcessorService.startService(appContext, ProcessorService.ProcessorConfig().apply {
+            sensorObserver = object :ProcessorService.ProcessorObserver {
+                override fun onOverloaded() {
+                    // your code here...
+                }
+
+                override fun onIdle() {
+                    // your code here...
+                }
+
+                override fun onChanged(data: ProcessorData) {
+                    // your code here...
+                }
+
+            }
+            dbType = Engine.DatabaseType.ROOM
+        })
+    }
+}
